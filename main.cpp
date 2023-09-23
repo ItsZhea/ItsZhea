c33f19135337991ba9220d69aa047cccb564463a
ItsZhea <145738766+ItsZhea@users.noreply.github.com>
2023-09-23 02:54:01 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>                       // IO Library
+using namespace std;                      //Use the standard IO Library
+int main()                                //Tells it start of main routine
+{                                         //Start of main routine
+    cout << "Hello, World!" << endl;      //Print out hello world
+    return 0;                             //Stop program, tell OS good run
+}                                         // end of the main routine
